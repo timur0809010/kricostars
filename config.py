1cc51ed0b8c7ea83147ba486a6f2d6a455c4504b
timur0809010 <tamerlan090980@gmail.com>
2025-04-11 22:51:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '8197482704:AAHlevw_pJE27ha3fvb3l7cYotXRg9pI-Cw'
+TOKEN = '7623841408:AAG1I5MemPWxh03eJ2cw7g_fl2Er8j57MdU'
 BOT_USERNAME = "krikostars_bot"  # Замените на ваше имя бота
 ADMIN_ID = 1613230792  # Замените на ID вашего администратора
 CHANNELS = [
